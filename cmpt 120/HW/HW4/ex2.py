a404a940876cba9b731381a51840b166c6fa4ca1
macklin-tsang <110071654+macklin-tsang@users.noreply.github.com>
2023-02-06 19:14:56 -0800

    Update ex2.py

--- cmpt 120/HW/HW4/ex2.py
+++ cmpt 120/HW/HW4/ex2.py
@@ -1,4 +1,4 @@
-# Date Month Year
+# Date Month Year Formatter
 # Author: Macklin Tsang
 # Date: January 30, 2023
 
@@ -21,10 +21,14 @@ months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Augus
 
 # 4. Print the converted format.
 
-# For looking up the months in the list: Grab the first string (which is the month in number form), convert it into an integer, and use that key number to look up the month in the months list.
+# For looking up the months in the list: Grab the first string (which is the month in number form), convert it into an integer, 
+# and use that key number to look up the month in the months list.
+
 # A slight issue will occur, since the list count starts with the first string being recognized at position #0.
 # For example, if the input month integer is 1 (January), it will print February which is technically in the "#1" position in the list, but is not the first month of the year.
 # To resolve this issue, we will subtract the integer by 1, so that it will return the month in our intended position.
-# For example, if the input month integer is 1 (January), it will subtract that integer by 1, resulting in 0. This will print January which is technically "#0" position in the list, but is the first month of the year.
+
+# For example, if the input month integer is 1 (January), it will subtract that integer by 1, resulting in 0. 
+# This will print January which is technically "#0" position in the list, but is the first month of the year.
 
 print(months[int(date[0])-1] + ' ' + date[-2] + ', ' + date[-1])
