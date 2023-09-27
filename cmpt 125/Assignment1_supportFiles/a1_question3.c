3ad5f814ce84dbe5866dcc2717951c1095e5243b
macklin-tsang <110071654+macklin-tsang@users.noreply.github.com>
2023-09-27 02:12:01 -0700

    Update a1_question3.c

--- cmpt 125/Assignment1_supportFiles/a1_question3.c
+++ cmpt 125/Assignment1_supportFiles/a1_question3.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
 #include "a1_question3.h"
 
-double squareNumber(number) {
+int squareNumber(int number) {
     return number * number;
 }
 
-double squareRoot(double number) {
+double squareRoot(int number) {
     return sqrt(number);
 }
 
 float printPointsDistances(unsigned int row, int points[][3], int point[]) {
 
     double totalEuclideanDistance = 0;
-
-    printf("This is the numbers for the 1st row: [%d, %d, %d]\n", points[0][0], points[0][1], points[0][2]);
-    printf("This is the numbers for the 2nd row: [%d, %d, %d]\n", points[1][0], points[1][1], points[1][2]);
-    printf("This is the numbers for the 3rd row: [%d, %d, %d]\n", points[2][0], points[2][1], points[2][2]);
-    printf("This is the numbers for the 4th row: [%d, %d, %d]\n", points[3][0], points[3][1], points[3][2]);
 
     for (int i = 0; i < row; i++) {
 
@@ -35,6 +30,8 @@ float printPointsDistances(unsigned int row, int points[][3], int point[]) {
 
         printf("Euclidean distance from (%d, %d, %d) to (%d, %d, %d) is %f\n", points[i][0],points[i][1],points[i][2], point[0], point[1], point[2], euclideanDistance);
     }
+
+    printf("This is the totalEuclideanDistance: %f\n", totalEuclideanDistance);
 
     return totalEuclideanDistance;
 }
